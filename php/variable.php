19f3bde43b0ee90292523926986efeef0a931a46
Coding-Cow <kbp0237@gmail.com>
2020-05-21 23:34:51 +0900

    variable egoing php

---
+++ php/variable.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name = "leezche";
+
+    echo "Lorem ipsum dolor sit amet, consectetur ".$name." adipisicing elit, sed do eiusmod tempor incididunt ut labore ".$name." et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit egoing in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat ".$name." cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    ?>
+  </body>
+</html>
