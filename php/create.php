2397c66b93c42dc4f9ea1f281d9cc09a90fced8c
cow-coding <kbp0237@gmail.com>
2020-10-10 00:29:21 +0900

    egoing php cru

--- php/create.php
+++ php/create.php
@@ -45,13 +45,16 @@ function print_list() {
         print_list();
       ?>
     </ol>
-    <h2>
-      <?php
-      print_title();
-      ?>
-    </h2>
-    <?php
-    print_description();
-    ?>
+    <a href="create.php">create</a>
+    <form action="create_process.php" method="post">
+        <p>
+          <input type="text" name="title" placeholder="Title">
+        </p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+          <input type="submit">
+    </form>
+
   </body>
 </html>
--- php/update.php
+++ php/update.php
@@ -49,13 +49,16 @@ function print_list() {
   <?php if(isset($_GET['id'])) {?>
     <a href="update.php?id=<?=$_GET['id']?>">update</a>
   <?php } ?>
-    <h2>
-      <?php
-      print_title();
-      ?>
-    </h2>
-    <?php
-    print_description();
-    ?>
+
+    <form action="update_process.php" method="post">
+          <input type="hidden" name="old_title" value="<?=$_GET['id']?>">
+        <p>
+          <input type="text" name="title" placeholder="Title" value="<?php print_title(); ?>">
+        </p>
+        <p>
+          <textarea name="description" placeholder="description" ><?php print_description(); ?></textarea>
+        </p>
+          <input type="submit">
+    </form>
   </body>
 </html>
