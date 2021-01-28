1dd607c4923cff768eb3c908b7e87716f7a6be33
cow-coding <kbp0237@gmail.com>
2021-01-28 19:56:11 +0900

    egoing node js

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -5,44 +5,67 @@ var url = require('url'); // module
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
+    var pathname = url.parse(_url, ture).pathname;
 
-    console.log(queryData.id);
-
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-
-    if(_url == '/favicon.ico'){
+    if (pathname == '/') {
+      if (queryData.id === undefined) {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+          var title = 'Welcome';
+          var description = 'Hello, Node.js'
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
+    } else {
       response.writeHead(404);
-      response.end();
-      return;
+      response.end('Not found');
     }
 
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data) {
-      var description = data;
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    });
+
 
 });
 app.listen(3000);
