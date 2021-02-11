a898594e5e602e266c97aa546df4b6f7ac080f3d
cow-coding <kbp0237@gmail.com>
2021-02-11 23:48:09 +0900

    ..

--- Nodejs/NodeJS Practice/router/main.js
+++ Nodejs/NodeJS Practice/router/main.js
@@ -3,6 +3,8 @@ module.exports = function (app, fs) {
   app.get("/", function (req, res) {
     var sess = req.session;
 
+    console.log(sess);
+
     res.render("index", {
       title: "MY HOMEPAGE",
       length: 5,
